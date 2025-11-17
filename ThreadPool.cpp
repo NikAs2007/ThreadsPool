8780f6e7241e0bc86d22e04791d7b9deadeb1fc4
SashaWindows <nikonorovsasha1@gmail.com>
2025-11-17 10:45:26 +0300

    Первая рабочая версия ThreadPool!

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,7 +1,29 @@
 #include "ThreadPool.h"
 
 ThreadPool::ThreadPool(int n) {
+	for (int i = 0; i < n; ++i) {
+		threads.emplace_back([this]() {
+			while (true) {
+				function<void()> task;
 
+				{
+					unique_lock<mutex> lock(mtx);
+					cv.wait(lock, [this]() { return stop || !tasks.empty(); });
+
+					if (stop && tasks.empty()) {
+						return;
+					}
+
+					task = move(tasks.front());
+					tasks.pop();
+
+				}
+
+				task();
+			}
+
+			});
+	}
 }
 
 void ThreadPool::emplace(function<void()>&& task) {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 ﻿#include "ThreadPool.h"
 
+#include <iostream>
+
 int main()
 {
-    
+    ThreadPool thread_pool;
+
+    thread_pool.emplace([]() { cout << "Hi"; });
+
+    return 0;
 }
